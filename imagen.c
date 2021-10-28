20b2daafebf4d809d15efd4b0046c2e423e620b9
Alan <69608277+alan1996colo@users.noreply.github.com>
2021-10-28 14:38:55 -0300

    Enmascarado listo.
    Se soluciono el problema de enmascarado, ademas se agrego una excepcion para que el programa corte si no se pasan imagenes del mismo tamaño. 
    *Se renombraron las funciones y parametros a como se pide en el enunciado que se llamen*
    Se agrego una variable int largoimg para guardar el largo de imagenes y comparar despues.
    En la funcion abrir se agrego una comparacion de largos de imagenes, en la cual si no soniguales tira un ERROR y corta el programa.

--- imagen.c
+++ imagen.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 //declaracion de variables globales
-//int ancho,alto; //no se usa por el momento
+
 void procesarArchivos(char *imagen1, char *imagen2, char *mask, int ancho, int alto);
-void enmascararImagen(unsigned char *a, unsigned char *b, unsigned char *mask, int cant);
+void enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, int cant);
 void abrirImagen(char *nombreArchivo, int tamano, unsigned char *buffer);
 void guardarImagen(char *nombreArchivo, int tamano, unsigned char *buffer);
-//void leerimagen(char* nombreArchivo); //no se usan por el momento
-//void imprimirimagen(int R[ancho][alto],int G[ancho][alto],int B[ancho][alto]); //no se usan por el momento
 /* programa para lectura y escritura de imagenes rgb o ppm en C*/
+int largoimg;
 int main(int argc, char *argv[])
 {
   clock_t tiempo_inicio, tiempo_final;
@@ -23,18 +22,13 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-
   //Parametros
   char *imagen1 = argv[1];      //imagene1
   char *imagen2 = argv[2];   //imagen2
   char *mask = argv[3];      //mascara
   int ancho = atoi(argv[4]); //para pasar el argumento a int
   int alto = atoi(argv[5]);  //para pasar el argumento a int
-
-  
-
  
-
   tiempo_final = clock();
 procesarArchivos(imagen1,imagen2,mask,ancho,alto);
   segundos = (double)(tiempo_final - tiempo_inicio) / CLOCKS_PER_SEC;
@@ -42,28 +36,32 @@ procesarArchivos(imagen1,imagen2,mask,ancho,alto);
 
   return 1;
 }
-/*
-void imprimirimagen(int R[ancho][alto],int G[ancho][alto],int B[ancho][alto]){
-    //imprime una imagen tomando cmoo parametro 3 arreglos de entero r,g,b y lo guarda en salida.ppm , es como el otro programa."imagenbpm"
-    FILE *ap;
-    ap=fopen("salida.ppm","w");
-    fprintf(ap,"P3\n%d %d\n255\n",ancho,alto);
-    int i,j;
-    for(i=0;i<alto;i++){
-        for(j=0;j<ancho;j++){
-            fprintf(ap,"%d %d %d ",R[i][j],G[i][j],B[i][j]);
-        }
-        fprintf(ap,"\n");
-    }
-    fclose(ap);
-}
-*/
+
 void abrirImagen(char *nombreArchivo, int tamano, unsigned char *buffer)
 {
   FILE *fp;
-  printf("abrir el archivo %s\n", nombreArchivo);
+  printf("abriendo el archivo %s\n", nombreArchivo);
 
-  fp = fopen(nombreArchivo, "r");//indicamos lectura
+  fp = fopen(nombreArchivo, "rb");//indicamos lectura
+  fseek(fp,0L,SEEK_END);//voy al inicio del archivo
+  if (largoimg==0){
+    largoimg=ftell(fp);//aviso el largo
+    fseek(fp, 0L, SEEK_SET);//vuelvo al principio
+    
+  }
+  else if (largoimg!=0){
+    if(largoimg!=ftell(fp)){
+      printf("ERROR:los archivos no tienen el mismo tamaño\n");
+      printf("PROGRAMA FINALIZADO\n");
+      exit(-1);
+      }
+    else {
+      largoimg=ftell(fp);//aviso el largo
+      fseek(fp, 0L, SEEK_SET);//vuelvo al principio
+   }
+  }
+  
+
   if (fp == NULL)//si no existe..
   {
     printf("Error al abrir el archivo\n");
@@ -72,6 +70,7 @@ void abrirImagen(char *nombreArchivo, int tamano, unsigned char *buffer)
   {
     fread(buffer, tamano, 1, fp);
   }
+  
   fclose(fp);
 }
 
@@ -84,9 +83,9 @@ void guardarImagen(char *nombreArchivo, int tamano, unsigned char *buffer)
   fclose(fp);
 }
 
-void enmascararImagen(unsigned char *a, unsigned char *b, unsigned char *mask, int tam)
+void enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, int cant)
 {
-  for (int i = 0; i < tam; i++)
+  for (int i = 0; i < cant; i++)
   {
     if (mask[i] != 0)
     { //0 si es negro - 255 si es blanco
@@ -99,21 +98,21 @@ void procesarArchivos(char *imagen1, char *imagen2, char *mask, int ancho, int a
 {
 
   int cantidadxPixel = 3; //RGB 3 bytes por pixel, un byte hasta 255 por cada color R,G O B
-  int tam = ancho * alto * cantidadxPixel;
+  int cant = ancho * alto * cantidadxPixel;
 
   //Reservar memoria dinámica
-  unsigned char *memoriaParaImg1 = malloc(tam);
-  unsigned char *memoriaParaImg2 = malloc(tam);
-  unsigned char *mascara = malloc(tam);
+  unsigned char *memoriaParaImg1 = malloc(cant);
+  unsigned char *memoriaParaImg2 = malloc(cant);
+  unsigned char *mascara = malloc(cant);
 
   //Abrimos las imagenes
-  abrirImagen(imagen1, tam, memoriaParaImg1);
-  abrirImagen(imagen2, tam, memoriaParaImg2);
-  abrirImagen(mask, tam, mascara);
+  abrirImagen(imagen1, cant, memoriaParaImg1);
+  abrirImagen(imagen2, cant, memoriaParaImg2);
+  abrirImagen(mask, cant, mascara);
 
-  enmascararImagen(memoriaParaImg1, memoriaParaImg2, mascara, tam);
-  char* nameImagen="ImagenConMascara.rgb";
-    guardarImagen(nameImagen, tam, memoriaParaImg1);
+  enmascarar_c(memoriaParaImg1, memoriaParaImg2, mascara, cant);
+  char* nameImagen="salida_c.rgb";
+    guardarImagen(nameImagen, cant, memoriaParaImg1);
 
   //Liberar memoria
   free(memoriaParaImg1);
@@ -121,34 +120,8 @@ void procesarArchivos(char *imagen1, char *imagen2, char *mask, int ancho, int a
   free(mascara);
 }
 
-
-
-
-/*
-void leerimagen(char *nombreArchivo){
-    //lee la imagen del archivo pasado en parametro, crea 3 arreglos :r,g,b con sus psiciones 
-    //alto y ancho y finalmente usa esos valores para pasarselos a imprimir imagen.
-    FILE *archivo;
-    archivo =fopen(nombreArchivo,"r");
-    char letra; int tipo,maxcolor;
-    fscanf(archivo,"%c %d %d %d %d",&letra,&tipo,&ancho,&alto,&maxcolor);
-    int R[ancho][alto],G[ancho][alto],B[ancho][alto];
-    int i,j;
-    for(i=0;i<alto;i++){
-        for(j=0;j<ancho;j++){
-            fscanf(archivo,"%d %d %d",&R[i][j],&G[i][j],&B[i][j]);
-        }
-    }
-fclose(archivo);
-imprimirimagen(R,G,B);
-
-}*/
-
-
-
 //para probar.
 //gcc imagen.c -o a.out
-
-//./a.out imgen.rgb imgverde.rgb masc.rgb 400 400
-//va producir un archivo llamado "ImagenConMascara.rgb"
+//./a.out avengers.bmp ligaj.bmp mascara.bmp 1920 1080
+//va producir un archivo llamado "imagen_c.rgb"
 //abrir y ver resultados.
